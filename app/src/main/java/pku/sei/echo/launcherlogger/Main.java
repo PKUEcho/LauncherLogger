08fb5a0be97d3285d80bc48f6e555dd965526464
echo <ec.pku.ho@gmail.com>
2016-06-21 09:09:49 +0800

    Merge branch 'master' of https://github.com/PKUEcho/LauncherLogger
    # Conflicts:
    #	app/src/main/java/pku/sei/echo/launcherlogger/Main.java

--- app/src/main/java/pku/sei/echo/launcherlogger/Main.java
+++ app/src/main/java/pku/sei/echo/launcherlogger/Main.java
@@ -1,10 +1,16 @@
 package pku.sei.echo.launcherlogger;
 
-import android.os.Bundle;
-import android.os.SystemClock;
-import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
+
+import android.app.ActivityManager;
+import android.app.AndroidAppHelper;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.*;
+
+import org.apache.commons.lang3.StringUtils;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -12,155 +18,129 @@ import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
-import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 
 public class Main implements IXposedHookLoadPackage {
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         XposedBridge.log("Loaded app: " + lpparam.packageName);
+        hookPackageManager(lpparam);
+        hookIntentAction(lpparam);
+        hookActivityLifecycle(lpparam);
+        hookBroadcast(lpparam);
+    }
 
-        if (lpparam.packageName.equals("com.android.launcher3")) {
-            String[] mName = {"onClickAppShortcut", "onClickFolderIcon", "startAppShortcutOrInfoActivity"};
-            for (String method : mName)
-                XposedHelpers.findAndHookMethod("com.android.launcher3.Launcher", lpparam.classLoader, method, View.class, new XC_MethodHook() {
+    public void hookPackageManager(final XC_LoadPackage.LoadPackageParam lpparam) {
+        if (!lpparam.packageName.equals("android"))
+            return;
+        XposedHelpers.findAndHookMethod(
+                "com.android.server.pm.PackageManagerService",
+                lpparam.classLoader,
+                "getPackageInfo",
+                String.class, int.class, int.class, new XC_MethodHook() {
                     @Override
                     protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        mLog(param, "Launch_S1," + ((TextView) param.args[0]).getText());
+                        String targetPackage = (String) param.args[0];
+                        int flags = (int) param.args[1];
+                        int userId = (int) param.args[2];
+                        formatPrt(lpparam, "getPackageInfo",
+                                "{'targetPackage': '" + targetPackage + "', "
+                                        + "'flags': " + flags + ", "
+                                        + "'userId': " + userId + "}");
                         super.beforeHookedMethod(param);
+                    }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        super.afterHookedMethod(param);
                     }
                 });
-        }
+        XposedHelpers.findAndHookMethod(
+                "com.android.server.pm.PackageManagerService",
+                lpparam.classLoader,
+                "getInstalledPackages",
+                int.class, int.class, new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        formatPrt(lpparam, "getInstalledPackages", "");
+                        super.beforeHookedMethod(param);
+                    }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        super.afterHookedMethod(param);
+                    }
+                });
+    }
+
+    public void hookIntentAction(final XC_LoadPackage.LoadPackageParam lpparam) {
+        Class<?> cls = XposedHelpers.findClass("android.app.ContextImpl", lpparam.classLoader);
+        XposedHelpers.findAndHookMethod(cls, "startActivity", Intent.class, Bundle.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                Intent intent = (Intent) param.args[0];
+                formatPrt(lpparam, "startActivity", "'" + intent.toUri(0) + "'");
+                super.beforeHookedMethod(param);
+            }
+
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                super.afterHookedMethod(param);
+            }
+        });
+        XposedHelpers.findAndHookMethod(cls, "startService", Intent.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                Intent intent = (Intent) param.args[0];
+                formatPrt(lpparam, "startService", "'" + intent.toUri(0) + "'");
+                super.beforeHookedMethod(param);
+            }
+
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                super.afterHookedMethod(param);
+            }
+        });
+        XposedHelpers.findAndHookMethod(cls, "bindService", Intent.class, ServiceConnection.class, int.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                Intent intent = (Intent) param.args[0];
+                formatPrt(lpparam, "bindService", "'" + intent.toUri(0) + "'");
+                super.beforeHookedMethod(param);
+            }
+
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                super.afterHookedMethod(param);
+            }
+        });
+    }
+
+    public void hookActivityLifecycle(final XC_LoadPackage.LoadPackageParam lpparam) {
         XposedHelpers.findAndHookMethod(
                 "android.app.Activity",
                 lpparam.classLoader,
-                "performCreate",
-                Bundle.class,
-                new XC_MethodHook() {
+                "onCreate",
+                Bundle.class, new XC_MethodHook() {
                     @Override
                     protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        mLog(param, "Launch_S4,");
+                        formatPrt(lpparam, "onCreate", "'");
                         super.beforeHookedMethod(param);
                     }
 
                     @Override
                     protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        mLog(param, "Launch_S5,");
                         super.afterHookedMethod(param);
                     }
-                }
-        );
-        XposedHelpers.findAndHookMethod(
-                "android.app.Activity",
-                lpparam.classLoader,
-                "performResume",
-                new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        mLog(param, "Launch_S6,");
-                        super.beforeHookedMethod(param);
-                    }
-
-                    @Override
-                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        mLog(param, "Launch_S7,");
-                        super.afterHookedMethod(param);
-                    }
-                }
-        );
-        XposedHelpers.findAndHookMethod(
-                "android.view.ViewRootImpl",
-                lpparam.classLoader,
-                "performTraversals",
-                new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        // mLog(param, "Launch_S8," + Log.getStackTraceString(new Exception()));
-                        mLog(param, "Launch_S8,");
-                        super.beforeHookedMethod(param);
-                    }
-
-                    @Override
-                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        mLog(param, "Launch_S9,");
-                        super.afterHookedMethod(param);
-                    }
-                }
-        );
-        if (lpparam.packageName.equals("android")) {
-            Class PRecord = XposedHelpers.findClass("com.android.server.am.ProcessRecord", lpparam.classLoader);
-            XposedHelpers.findAndHookMethod(
-                    "com.android.server.am.ActivityManagerService",
-                    lpparam.classLoader,
-                    "startProcessLocked",
-                    PRecord,
-                    String.class,
-                    String.class,
-                    String.class,
-                    String.class,
-                    String[].class,
-                    new XC_MethodHook() {
-                        @Override
-                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                            mLog(param, "Launch_S2," + param.args[2]);
-                            super.beforeHookedMethod(param);
-                        }
-
-                        @Override
-                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                            super.afterHookedMethod(param);
-                        }
-                    }
-            );
-
-            XposedHelpers.findAndHookMethod(
-                    "com.android.server.am.ActivityRecord",
-                    lpparam.classLoader,
-                    "reportLaunchTimeLocked",
-                    long.class,
-                    new XC_MethodHook() {
-                        @Override
-                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                            super.beforeHookedMethod(param);
-                        }
-
-                        @Override
-                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                            mLog(param, "Launch_S10,");
-                            super.afterHookedMethod(param);
-                        }
-                    }
-            );
-
+                });
+        String[] hook_methods = {"onStart", "onRestart", "onResume", "onPause", "onStop", "onDestroy"};
+        for (final String method: hook_methods) {
             XposedHelpers.findAndHookMethod(
                     "android.app.Activity",
                     lpparam.classLoader,
-                    "onCreate",
-                    Bundle.class,
+                    method,
                     new XC_MethodHook() {
                         @Override
                         protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                            mLog(param, "Launch_S4,");
-                            super.beforeHookedMethod(param);
-                        }
-
-                        @Override
-                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                            mLog(param, "Launch_S5,");
-                            super.afterHookedMethod(param);
-                        }
-                    }
-            );
-
-            // in frameworks/base/core/java/android/app/ActivityThread.java
-            // attach(boolean system)
-            /*XposedHelpers.findAndHookMethod(
-                    "android.app.ActivityThread",
-                    lpparam.classLoader,
-                    "attach",
-                    boolean.class,
-                    new XC_MethodHook() {
-                        @Override
-                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                            mLog(param, "Launch_S3," + param.args[0]);
+                            formatPrt(lpparam, method, "'");
                             super.beforeHookedMethod(param);
                         }
 
@@ -168,34 +148,84 @@ public class Main implements IXposedHookLoadPackage {
                         protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                             super.afterHookedMethod(param);
                         }
-                    }
-            );*/
-
-            // in framework/base/core/java/com/android/internal/os/Zygote.java
-            // public static int forkAndSpecialize(int uid, int gid, int[] gids, int debugFlags,
-            //      int[][] rlimits, int mountExternal, String seInfo, String niceName, int[] fdsToClose,
-            //      String instructionSet, String appDataDir)
-            /*XposedHelpers.findAndHookMethod(
-                    "com.android.internal.os.Zygote",
-                    lpparam.classLoader,
-                    "forkAndSpecialize",
-                    int.class, int.class, int[].class, int.class, int[][].class, int.class, String.class, String.class, int[].class, String.class, String.class,
-                    new XC_MethodHook() {
-                        @Override
-                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                            mLog(param, "Launch_S3," + param.args[7]);
-                            super.beforeHookedMethod(param);
-                        }
-
-                        @Override
-                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                            super.afterHookedMethod(param);
-                        }
-                    }
-            );*/
+                    });
         }
     }
-    private void mLog(XC_MethodHook.MethodHookParam param, String str) {
-        XposedBridge.log(SystemClock.elapsedRealtime() + "," + param.method.getName() + "," + str);
+
+    public void hookBroadcast(final XC_LoadPackage.LoadPackageParam lpparam) {
+        Class<?> cls = XposedHelpers.findClass("android.app.ContextImpl", lpparam.classLoader);
+        XposedBridge.hookAllMethods(cls, "sendBroadcast", new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                Intent intent = (Intent) param.args[0];
+                formatPrt(lpparam, "sendBroadcast", "{'IntentUri': '" + intent.toUri(0) + "'}");
+//                formatPrt(lpparam, "broadcastIntent",
+//                        "{'IntentUri': '" + intent.toUri(0) + "', "
+//                                + "'requestCode': " + param.args[4] + ", "
+//                                + "'resultData': '" + param.args[5] + "', "
+//                                + "'requiredPerm': " + param.args[7] + "'}");
+                        super.beforeHookedMethod(param);
+            }
+
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                super.afterHookedMethod(param);
+            }
+        });
+
+        XposedBridge.hookAllMethods(cls, "registerReceiver", new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                BroadcastReceiver receiver = (BroadcastReceiver) param.args[0];
+                IntentFilter filter = (IntentFilter) param.args[1];
+                String actions = "";
+                for (int i = 0; i < filter.countActions(); ++ i)
+                    actions += (filter.getAction(i) + ",");
+                String categories = "";
+                for (int i = 0; i < filter.countCategories(); ++ i)
+                    categories += (filter.getCategory(i) + ",");
+                formatPrt(lpparam, "registerReceiver",
+                        "{'action': '" + actions + "', "
+                                + "'categories': " + categories + "'}");
+                super.beforeHookedMethod(param);
+            }
+
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                super.afterHookedMethod(param);
+            }
+        });
+    }
+
+    private String intent2desc(Intent intent) {
+        return "{'package': '" + intent.getComponent().getPackageName() + "', "
+                + "'class': '" + intent.getComponent().getClassName() + "', "
+                + "'action': '" + intent.getAction() + "', "
+                + "'type': '" + intent.getType() + "', "
+                + "'categories': '" + StringUtils.join(intent.getCategories().toArray()) + "', "
+                + "'Uri': '" + intent.toUri(0) + "'";
+    }
+
+    private String getProcessName(Context ctx) {
+        String currentProcName = "";
+        int pid = android.os.Process.myPid();
+        ActivityManager manager = (ActivityManager) ctx.getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningAppProcessInfo processInfo : manager.getRunningAppProcesses())
+        {
+            if (processInfo.pid == pid)
+            {
+                currentProcName = processInfo.processName;
+                break;
+            }
+        }
+        return currentProcName;
+    }
+
+    private void formatPrt(final XC_LoadPackage.LoadPackageParam lpparam, String action, String desc) {
+        Context context = AndroidAppHelper.currentApplication();
+        XposedBridge.log("{'package': '" + context.getPackageName() + "', "
+                + "'process': '" + getProcessName(context) + "', "
+                + "'method': '" + action + "', "
+                + "'desc': " + desc);
     }
 }
